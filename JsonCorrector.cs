4823c152c0bd60d8c4d42b93d71ef40c568fb2c7
Denisdeparture <denis11you777602@gmail.com>
2024-08-23 20:41:58 +0300

    AddJsonCorrector
    Проверяет данныые на валидность

---
+++ JsonCorrector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSoundcloudApi
+{
+    internal static class JsonCorrector
+    {
+        internal static string TryJson(HttpResponseMessage response, out string json)
+        {
+            if (response is null) throw new NullReferenceException($"Resonse  was null");
+            if (!response.IsSuccessStatusCode) throw new Exception($"Resonse  was {response.StatusCode} is incorrect");
+            var task = response.Content.ReadAsStringAsync();
+            task.Wait();
+            json = task.Result;
+            if (string.IsNullOrWhiteSpace(json)) throw new NullReferenceException(nameof(json));
+            return json;
+        }
+    }
+}
