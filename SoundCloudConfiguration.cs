f93b552f82e6a4fcb68790d9f9712f8fb1920dcb
Denisdeparture <denis11you777602@gmail.com>
2024-08-23 20:43:14 +0300

    AddBasicConfModel

---
+++ SoundCloudConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSoundcloudApi
+{
+    public record SoundCloudConfiguration(string OAuthToken,string Protocol = "https", string DomenName = "soundcloud", string DomenRegion  = "com", string ApiVersion = "api-v2");
+}
